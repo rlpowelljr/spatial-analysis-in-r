071c381c51ef973dd02ad0a8ee2f6c6456de99cf
Robby Powell <rpowell.che06@gtalumni.org>
2016-07-13 22:10:02 -0500

    Updated file list for download.

--- scripts/bls.data.download.R
+++ scripts/bls.data.download.R
@@ -6,8 +6,7 @@ main.url <- "http://download.bls.gov/pub/time.series/la/"
 download.dir <- "~/spatial-analysis-in-r/data/bls/"
 
 # List of files to download
-file.list <- c("la.area", "la.area_type", "la.data.64.County",
-               "la.area", "la.area.type", "la.measure", "la.measure",
+file.list <- c("la.area", "la.area_type", "la.data.64.County", "la.measure",
                "la.period", "la.series", "la.state_region_division")
 
 # combine download urls
@@ -18,7 +17,9 @@ download.locations <- unlist(lapply(download.dir, FUN = "paste0", file.list))
 
 # download files
 for (file in 1:length(file.list)) {
-  download.file(url = download.urls[file], destfile = download.locations[file])
+  if (!(file.list[file] %in% dir(download.dir))) {
+    download.file(url = download.urls[file], destfile = download.locations[file])
+  }
 }
 
 
